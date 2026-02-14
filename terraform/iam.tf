05260a97eb571c71a4fda3ca3427b8b1aa8d68f7
jsh dncn <pythonicqualms@gmail.com>
2026-02-14 05:22:25 -0600

    remove last iam resource

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -62,8 +62,3 @@ resource "aws_iam_policy" "github_actions" {
     ]
   })
 }
-
-resource "aws_iam_role_policy_attachment" "github_actions" {
-  role       = aws_iam_role.github_actions.name
-  policy_arn = aws_iam_policy.github_actions.arn
-}
