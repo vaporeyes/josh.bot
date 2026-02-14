6e9e90f8864016383936d22b1d4edddc0fbe8f8e
jsh dncn <pythonicqualms@gmail.com>
2026-02-14 05:37:06 -0600

    switch to api key for real

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jduncan/josh-bot
 
 go 1.24.3
+
+require github.com/aws/aws-lambda-go v1.52.0 // indirect
--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,3 +1,6 @@
+# ABOUTME: Defines the Lambda function, HTTP API Gateway, and API key for josh-bot.
+# ABOUTME: API key auth is handled in Go code since HTTP APIs don't support native API keys.
+
 # 1. The Lambda Function
 resource "aws_lambda_function" "josh_bot_api" {
   filename      = "function.zip"
@@ -10,15 +13,15 @@ resource "aws_lambda_function" "josh_bot_api" {
   environment {
     variables = {
       APP_ENV = "production"
+      API_KEY = random_password.api_key.result
     }
   }
 }
 
 # 2. API Gateway (HTTP API - cheaper and faster than REST API)
 resource "aws_apigatewayv2_api" "josh_bot_gw" {
-  name                         = "josh-bot-gateway"
-  protocol_type                = "HTTP"
-  api_key_selection_expression = "$request.header.x-api-key"
+  name          = "josh-bot-gateway"
+  protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_integration" "lambda_link" {
@@ -31,8 +34,7 @@ resource "aws_apigatewayv2_integration" "lambda_link" {
 resource "aws_apigatewayv2_route" "default_route" {
   api_id           = aws_apigatewayv2_api.josh_bot_gw.id
   route_key        = "ANY /{proxy+}"
-  target           = "integrations/${aws_apigatewayv2_integration.lambda_link.id}"
-  api_key_required = true
+  target = "integrations/${aws_apigatewayv2_integration.lambda_link.id}"
 }
 
 # 4. Lambda Permission to allow API Gateway calls
@@ -44,23 +46,26 @@ resource "aws_lambda_permission" "api_gw" {
   source_arn    = "${aws_apigatewayv2_api.josh_bot_gw.execution_arn}/*/*"
 }
 
-# 5. API Key and Usage Plan
-resource "aws_apigatewayv2_api_key" "default" {
-  name   = "josh-bot-api-key"
-  api_id = aws_apigatewayv2_api.josh_bot_gw.id
+# 5. API Gateway Stage
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.josh_bot_gw.id
+  name        = "$default"
+  auto_deploy = true
 }
 
-resource "aws_apigatewayv2_usage_plan" "default" {
-  name   = "josh-bot-usage-plan"
-  api_id = aws_apigatewayv2_api.josh_bot_gw.id
+# 6. API Key (generated and stored in SSM, passed to Lambda as env var)
+resource "random_password" "api_key" {
+  length  = 40
+  special = false
 }
 
-resource "aws_apigatewayv2_usage_plan_key" "default" {
-  key_id        = aws_apigatewayv2_api_key.default.id
-  usage_plan_id = aws_apigatewayv2_usage_plan.default.id
+resource "aws_ssm_parameter" "api_key" {
+  name  = "/josh-bot/api-key"
+  type  = "SecureString"
+  value = random_password.api_key.result
 }
 
 output "api_key_value" {
-  value     = aws_apigatewayv2_api_key.default.value
+  value     = random_password.api_key.result
   sensitive = true
 }
