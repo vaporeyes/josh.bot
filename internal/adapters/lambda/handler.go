dd315ecf24d5a16c8b89437be54e76e973ed502c
jsh dncn <pythonicqualms@gmail.com>
2026-02-14 15:51:26 -0600

    formatting

--- internal/adapters/lambda/handler.go
+++ internal/adapters/lambda/handler.go
@@ -152,8 +152,8 @@ func jsonResponse(statusCode int, body string) events.APIGatewayProxyResponse {
 		StatusCode: statusCode,
 		Body:       body,
 		Headers: map[string]string{
-			"Content-Type":                "application/json",
-			"Access-Control-Allow-Origin": "*",
+			"Content-Type":                 "application/json",
+			"Access-Control-Allow-Origin":  "*",
 			"Access-Control-Allow-Headers": "Content-Type, x-api-key",
 			"Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
 		},
